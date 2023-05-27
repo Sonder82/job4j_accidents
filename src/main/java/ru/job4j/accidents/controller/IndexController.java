2d117ae890a4158c93887751721411398483db02
Sonder82 <Himrs82@gmail.com>
2023-05-27 15:20:47 +0500

    1. Загрузка данных в вид [#295782]

--- src/main/java/ru/job4j/accidents/controller/IndexController.java
+++ src/main/java/ru/job4j/accidents/controller/IndexController.java
@@ -1,6 +1,7 @@
 package ru.job4j.accidents.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -11,7 +12,8 @@ public class IndexController {
      * @return возвращает начальную страницу
      */
     @GetMapping({"/", "/index"})
-    public String getIndex() {
+    public String getIndex(Model model) {
+        model.addAttribute("user", "Petr Arsentev");
         return "index";
     }
 }
