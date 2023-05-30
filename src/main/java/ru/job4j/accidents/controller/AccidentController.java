593e1facd0dd02067c57747c5ae54ab27d989b1b
Sonder82 <Himrs82@gmail.com>
2023-05-30 12:06:43 +0500

    3. @ModelAttribute. Создание инцидента. [#261013]

--- src/main/java/ru/job4j/accidents/controller/AccidentController.java
+++ src/main/java/ru/job4j/accidents/controller/AccidentController.java
@@ -3,9 +3,11 @@ package ru.job4j.accidents.controller;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import ru.job4j.accidents.model.Accident;
 import ru.job4j.accidents.service.AccidentService;
+
+import java.util.Optional;
 
 @Controller
 @AllArgsConstructor
@@ -18,5 +20,42 @@ public class AccidentController {
     public String getAll(Model model) {
         model.addAttribute("accidents", accidentService.findAll());
         return "accidents/list";
+    }
+
+    @GetMapping("/createAccident")
+    public String viewCreateAccident() {
+        return "accidents/createAccident";
+    }
+
+    @PostMapping("/saveAccident")
+    public String save(@ModelAttribute Accident accident) {
+        accidentService.save(accident);
+        return "redirect:/accidents";
+    }
+
+    @GetMapping("/{id}")
+    public String getById(Model model, @PathVariable int id) {
+        Optional<Accident> accidentOptional = accidentService.findById(id);
+        if (accidentOptional.isEmpty()) {
+            model.addAttribute("message", "Инцидент с указанным идентификатором не найден");
+            return "errors/404";
+        }
+        model.addAttribute("accident", accidentOptional.get());
+        return "accidents/editAccident";
+    }
+
+    @PostMapping("/editAccident")
+    public String update(@ModelAttribute Accident accident, Model model) {
+        try {
+            boolean isUpdated = accidentService.update(accident);
+            if (!isUpdated) {
+                model.addAttribute("message", "Инцидент с указанным идентификатором не найден");
+                return "errors/404";
+            }
+            return "redirect:/accidents";
+        } catch (Exception exception) {
+            model.addAttribute("message", exception.getMessage());
+            return "errors/404";
+        }
     }
 }
